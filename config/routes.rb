601e7fa967816760128328152b9ba8e0d19f3c53
EC2 Default User <ec2-user@ip-172-31-36-125.ap-northeast-1.compute.internal>
2019-11-22 04:22:33 +0000

    finished study4

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   get "posts/:id" => "posts#show"
   # new.htmlの送信ボタンが押された時
   post "posts/create" => "posts#create"
+  # show.htmlの編集ボタンが押された時
+  get "posts/:id/edit" => "posts#edit"
+  # edit.htmlの保存ボタンが押された時
+  post "posts/:id/update" => "posts#update"
+  # show.htmlの削除ボタンが押された時
+  post "posts/:id/destroy" => "posts#destroy"
 
   get "/" => "home#top"
   get "about" => "home#about"
