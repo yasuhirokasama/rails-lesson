6693a0e29e7252edae658b8a2abbe0648e22063d
EC2 Default User <ec2-user@ip-172-31-36-125.ap-northeast-1.compute.internal>
2019-11-25 04:28:49 +0000

    finished study7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191124011526) do
+ActiveRecord::Schema.define(version: 20191125011925) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "content",    limit: 65535
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20191124011526) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image_name"
   end
 
 end
